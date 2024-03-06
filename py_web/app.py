73b4d2136e7fc3b0790497a1cda8afe6a5b2c71d
ihomya <imhom@outlook.com>
2024-03-06 11:57:59 +0800

    deleted:    __pycache__/app.cpython-38.pyc 	new file:   py_web/__pycache__/app.cpython-38.pyc 	renamed:    app.py -> py_web/app.py 	renamed:    music163.py -> py_web/music163.py 	new file:   py_web/static/cat.jpg 	new file:   py_web/static/cfg999999.json 	new file:   py_web/static/editor.css 	new file:   py_web/static/editor.js 	new file:   py_web/static/gh_16e4613251a6_258.jpg 	new file:   py_web/static/index.js 	new file:   py_web/static/jsonform.js 	new file:   py_web/static/jsonform.json 	new file:   py_web/static/jsonlint.js 	new file:   py_web/static/jsv.js 	renamed:    templates/admin.html -> py_web/templates/admin.html 	new file:   py_web/templates/editor.html 	renamed:    templates/index.html -> py_web/templates/index.html 	new file:   py_web/templates/layout.html 	renamed:    test.py -> py_web/test.py 	renamed:    test2.py -> py_web/test2.py 	deleted:    templates/editor.html 	deleted:    templates/layout.html

--- py_web/app.py
+++ py_web/app.py
@@ -3,10 +3,11 @@
 import json
 import web 
 urls = (
-    '/index[/]?.*', 'index',
-    '/editor[/]?.*', 'editor'
+    '/', 'index', # 将url交给index类处理 
+    '/editor[/]?.*', 'editor',
+    '/static/(.*)', 'StaticFile'
 ) 
-data = json.load(open('./dc.json','r') )
+data = json.load(open('../dc.json','r',encoding='utf-8') )
     
 def login(name,pwd):
     if name=="admin" & pwd == "admin":
@@ -21,11 +22,15 @@ class index:
 
 class editor:
     def GET(self): 
-        return render.editor()
+        return render.editor() 
 class admin:
     def GET(self): 
         return render.admin()
-    
+
+class StaticFile:
+    def GET(self, filename):  
+        web.seeother('/static/'+filename); #重定向
+
 app = web.application(urls, globals())
 
 if __name__ == "__main__":
